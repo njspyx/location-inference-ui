bc1d8b2f69ad41478f12089d191be3463e6b56d6
Neel Jay <neeljay03@gmail.com>
2024-10-15 22:05:31 -0400

    Fix images collection to allow full coverage

--- server/convert_to_json.py
+++ server/convert_to_json.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
-df = pd.read_csv('coordinates.csv')
+df = pd.read_csv('img_coordinates_final_v1_with_country.csv')
+# dedup based on 'filename' column
+df = df.drop_duplicates(subset='filename', keep='first')
+
 json_output = df.to_json(orient='records', lines=False)
 
 # save json file
--- server/upload_to_firebase.js
+++ server/upload_to_firebase.js
@@ -1,7 +1,7 @@
 // Script to upload benchmark json to firestore collection, which will be used to assign and query images in UI.
 
 const admin = require("firebase-admin");
-const serviceAccount = require("./apart-location-inference-f196f-firebase-adminsdk-y5t42-efe95c8dc4.json");
+const serviceAccount = require("./apart-location-inference-f196f-firebase-adminsdk-y5t42-239265f4de.json");
 const data = require("./coordinates.json");
 const fs = require("fs");
 
@@ -13,13 +13,18 @@ const firestore = admin.firestore();
 
 const uploadData = async () => {
   const batch = firestore.batch();
-  data.forEach((doc) => {
-    const docRef = firestore.collection("images").doc();
+  data.forEach((doc, index) => {
+    const docRef = firestore.collection("images").doc(index.toString()); // Set doc ID to index as string
     batch.set(docRef, doc);
   });
 
   await batch.commit();
   console.log("Data uploaded successfully.");
+
+  await firestore.collection("settings").doc("imageAssignment").set({
+    totalNumberOfImages: data.length,
+    nextImageIndex: 0,
+  });
 };
 
 uploadData().catch((error) => console.error("Error uploading data:", error));
